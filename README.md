ffac7926c3e807bf94c03c41bdbe743ee56460c7
ChoiHyunMin <chl1519@naver.com>
2021-06-27 20:34:57 +0900

    walrus0627

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-python3  stdy
+python3  study
  
--- walrus.py
+++ walrus.py
@@ -5,6 +5,9 @@ fresh_fruit = {
 }
 
 
+
+# 1
+
 def make_lemonade(count):
     print(count, "make lemonade")
 
@@ -12,7 +15,6 @@ def out_of_stock():
     print("out of stock")
 
 count = fresh_fruit.get('레몬', 0)
-
 
 # 기존
 # if count:
@@ -27,6 +29,8 @@ if count := fresh_fruit.get('레몬', 0):
 else:
     out_of_stock()
     
+
+# 2
     
 def make_cider(count):
     print(count, "make cider")
@@ -43,4 +47,61 @@ count = fresh_fruit.get('사과', 0)
 if (count := fresh_fruit.get('사과', 0)) >= 4:
     make_cider(count)
 else:
+    out_of_stock()
+    
+    
+# 3
+
+def slice_bananas(count):
+    pass
+
+class OutOfBananas(Exception):
+    pass
+
+def make_smoothies(count):
+    pass
+
+#1)
+pieces = 0
+count = fresh_fruit.get('바나나', 0)
+if count >= 2:
+    pieces = slice_bananas(count)
+    
+try:
+    smoothies = make_smoothies(pieces)
+except OutOfBananas:
+    out_of_stock()
+    
+#2)
+count = fresh_fruit.get('바나나', 0)
+if count >= 2:
+    pieces = slice_bananas(count)
+else:
+    pieces = 0
+    
+try:
+    smoothies = make_smoothies(pieces)
+except OutOfBananas:
+    out_of_stock()
+    
+    
+# Walrus 1
+pieces = 0
+if (count := fresh_fruit.get('바나나', 0)) >= 2:
+    pieces = slice_bananas(count)
+    
+try:
+    smoothies = make_smoothies(pieces)
+except OutOfBananas:
+    out_of_stock()
+    
+# Walrus 2
+if (count := fresh_fruit.get('바나나', 0)) >= 2:
+    pieces = slice_bananas(count)
+else:
+    pieces = 0
+    
+try:
+    smoothies = make_smoothies(pieces)
+except OutOfBananas:
     out_of_stock()
